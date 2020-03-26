d86bacbeb4519180c5d54f8f6b0271ab06c0baaf
Tuanngoc.n <tuanngocmobileapp@gmail.com>
2020-03-26 23:03:31 +0700

    add button FAB

--- app/src/main/java/com/example/mynoteenglish/MainActivity.java
+++ app/src/main/java/com/example/mynoteenglish/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mynoteenglish;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.KeyEventDispatcher;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
+public class MainActivity extends AppCompatActivity  {
+    FloatingActionButton flbAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        flbAdd = findViewById(R.id.fba_add);
+        setSupportActionBar(toolbar);
+        flbAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
