89c76fe0db9e067cfb92098411ab83f25ea1c1d0
Tuanngoc.n <tuanngocmobileapp@gmail.com>
2020-03-29 09:43:16 +0700

    add fuction OnclickListener

--- app/src/main/java/com/example/mynoteenglish/view/MainActivity.java
+++ app/src/main/java/com/example/mynoteenglish/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.mynoteenglish;
+package com.example.mynoteenglish.view;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,25 +8,37 @@ import androidx.core.view.KeyEventDispatcher;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.mynoteenglish.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
     FloatingActionButton flbAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        flbAdd = findViewById(R.id.fba_add);
-        setSupportActionBar(toolbar);
-        flbAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).show();
-            }
-        });
+        Mapping ();
+        SetEventOnclick();
     }
-
+    protected void SetEventOnclick()
+    {
+        flbAdd.setOnClickListener(this);
+    }
+    protected  void Mapping ()
+    {
+        flbAdd = findViewById(R.id.fba_add);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.fba_add:
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+    }
 }
