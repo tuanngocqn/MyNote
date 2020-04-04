f65bbdfb927d6bc80dd018ad2c5ece1716427df6
Tuanngoc.n <tuanngocmobileapp@gmail.com>
2020-04-04 16:02:16 +0700

    Modify lib texttospeedcomplete can split 4000 chars

--- app/src/main/java/com/example/mynoteenglish/viewmodel/LibTextToSpeedCompleted.java
+++ app/src/main/java/com/example/mynoteenglish/viewmodel/LibTextToSpeedCompleted.java
@@ -11,13 +11,14 @@ import android.widget.Toast;
 import com.example.mynoteenglish.R;
 import com.example.mynoteenglish.view.MainActivity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 
 public class LibTextToSpeedCompleted implements TextToSpeech.OnInitListener
 {
 
-    boolean repeat =false;
+    boolean repeat =false,checkstatus=false;
     Context mcontext;
     Activity mainActivity;
     private boolean initialized;
@@ -32,59 +33,38 @@ public class LibTextToSpeedCompleted implements TextToSpeech.OnInitListener
         tts.setOnUtteranceProgressListener(mProgressListener);
         Log.d(TAG,"TTS create");
     }
-//https://stackoverflow.com/questions/19312536/android-tts-fails-to-speak-large-amount-of-text
-//    int length = toSpeech.getMaxSpeechInputLength() - 1;
-//    Iterable<String> chunks = Splitter.fixedLength(length).split(largeText);
-//Lists.newArrayList(chunks);
-//private void speech(String charSequence) {
-//
-//    int position ;
-//
-//
-//    int sizeOfChar= charSequence.length();
-//    String testStri= charSequence.substring(position,sizeOfChar);
-//
-//
-//    int next = 20;
-//    int pos =0;
-//    while(true) {
-//        String temp="";
-//        Log.e("in loop", "" + pos);
-//
-//        try {
-//
-//            temp = testStri.substring(pos, next);
-//            HashMap<String, String> params = new HashMap<String, String>();
-//            params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, temp);
-//            engine.speak(temp, TextToSpeech.QUEUE_ADD, params);
-//
-//            pos = pos + 20;
-//            next = next + 20;
-//
-//        } catch (Exception e) {
-//            temp = testStri.substring(pos, testStri.length());
-//            engine.speak(temp, TextToSpeech.QUEUE_ADD, null);
-//            break;
-//
-//        }
-//
-//    }
-//
-//}
     public void speak(String text) {
-
+        checkstatus=false;
         if (!initialized) {
             queuedText = text;
             return;
         }
+        String afterSubstring=text;
+        ArrayList<String> arrayList= new ArrayList<>();
+        int maxlength=tts.getMaxSpeechInputLength()-1;
+//        int position=0;
+//        int next =maxlength;
+        HashMap<String, String> map;// su dung cai nay de cho co the dung detect completed!@
         queuedText = null;
+       while (true) {
+           try {
+               String stringTospeak= afterSubstring.substring(0, maxlength);
+               afterSubstring= afterSubstring.substring(maxlength);
+               map= new HashMap<String, String>();
+               map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+               tts.speak(stringTospeak, TextToSpeech.QUEUE_ADD,map);
+           }
+           catch (Exception e)
+           {
+               checkstatus=true;
+               map= new HashMap<String, String>();
+               map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+               tts.speak(afterSubstring, TextToSpeech.QUEUE_ADD,map);
+               break;
+           }
 
-        setTtsListener(); // no longer creates a new UtteranceProgressListener each time
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
-        tts.speak(text, TextToSpeech.QUEUE_ADD, map);
+       }
         textTospeak= text;
-        Log.d(TAG,tts.getMaxSpeechInputLength()+"");
     }
     public  void SetLanguage(Locale locale)
     {
@@ -121,9 +101,6 @@ public class LibTextToSpeedCompleted implements TextToSpeech.OnInitListener
     private abstract class runnable implements Runnable {
     }
 
-
-
-
     private UtteranceProgressListener mProgressListener = new UtteranceProgressListener() {
         @Override
         public void onStart(String utteranceId) {
@@ -145,7 +122,7 @@ public class LibTextToSpeedCompleted implements TextToSpeech.OnInitListener
                         public void run()
                         {
 
-                            if (repeat)
+                            if (repeat && checkstatus)
                             {
                                 speak(textTospeak);
 
