975730048cab67968d13e82e9377d72b0c78688b
Tuanngoc.n <tuanngocmobileapp@gmail.com>
2020-04-12 21:56:29 +0700

    add new Mainanimation\

---
+++ app/src/main/java/com/example/mynoteenglish/view/MainAnimation.java
@@ -0,0 +1,17 @@
+package com.example.mynoteenglish.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mynoteenglish.R;
+
+public class MainAnimation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_animation);
+    }
+
+}
